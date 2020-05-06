c9953d5907de76851802fcb88e15ab61cb9e3394
Eshi44 <e.hameliervolino@gmail.com>
2020-05-06 18:46:23 -0400

    new postbuild script

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,12 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/api/config", (req, res) => {
-    res.json({
-      success: true,
-      currentPort: PORT
-    });
-  });
+	res.json({
+		success: true,
+		currentPort: PORT,
+	});
+});
 
-  app.listen(PORT, () => {
-    console.log(`Express App is running on http://localhost:${PORT}`);
-  });
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+	console.log(`Express App is running on http://localhost:${PORT}`);
+});
