cedb8709961637ee1c0edd8bfaf6b5719e00da01
Eshi44 <e.hameliervolino@gmail.com>
2020-05-25 19:46:10 -0400

    added second row of skill carousel

--- client/src/components/Carousel/SkillsCarousel.jsx
+++ client/src/components/Carousel/SkillsCarousel.jsx
@@ -4,7 +4,10 @@ import "./SkillsCarousel.css";
 import NodeJS from "../../images/nodejs.png";
 import MySQL from "../../images/MySQL.png";
 import REACT from "../../images/reactjs.png";
-import $ from "jquery";
+import jQuery from "../../images/jquery.png";
+import Heroku from "../../images/heroku.png";
+import Express from "../../images/expressjs.png";
+import Handlebars from "../../images/handlebars.png";
 
 class SkillsCarousel extends Component {
 	render() {
@@ -12,51 +15,73 @@ class SkillsCarousel extends Component {
 			<>
 				<div
 					id="carouselExampleControls"
-					class="carousel slide "
+					className="carousel slide "
 					data-ride="carousel"
 					data-interval="false"
 				>
-					<div class="carousel-inner">
-						<div class="carousel-item active">
+					<div className="carousel-inner">
+						<div className="carousel-item active">
 							<div className="row">
-								<div class="col-3">
+								<div className="col-3">
 									<img
-                                    id="javascript"
-										class="d-block w-100"
-										src={JavascriptIcon}
-										alt="First slide"
-									/>
+										className="d-block w-100" src={JavascriptIcon} alt="Javascript"/>
 								</div>
-								<div class="col-3">
-									<img class="d-block w-100" src={NodeJS} alt="Second slide" />
+								<div className="col-3">
+									<img className="d-block w-100" src={NodeJS} alt="Nodejs" />
 								</div>
-								<div class="col-3">
+								<div className="col-3">
 									{" "}
-									<img class="d-block w-100" src={REACT} alt="Third slide" />
+									<img className="d-block w-100" src={REACT} alt="Reactjs" />
 								</div>
-								<div class="col-3">
-									<img class="d-block w-100" src={MySQL} alt="Third slide" />
+								<div className="col-3">
+									<img className="d-block w-100" src={MySQL} alt="MySQL" />
 								</div>
 							</div>
 						</div>
+                        <div className="carousel-item">
+							<div className="row">
+								<div className="col-3">
+                                <img className="d-block w-100" src={Heroku} alt="Heroku" />
+									
+								</div>
+								<div className="col-3">
+									<img id="handlebars" className="d-block w-100" src={Handlebars} alt="Handlebars" />
+								</div>
+								<div className="col-3">
+									{" "}
+									<img className="d-block w-100" src={jQuery} alt="jQuery" />
+								</div>
+								<div className="col-3">
+                                <img
+										className="d-block w-100" src={Express} alt="Expressjs"/>
+								</div>
+							</div>
+						</div>
+                        
 					</div>
 					<a
-						class="carousel-control-prev"
+						className="carousel-control-prev"
 						href="#carouselExampleControls"
 						role="button"
 						data-slide="prev"
 					>
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
+						<span
+							className="carousel-control-prev-icon"
+							aria-hidden="true"
+						></span>
+						<span className="sr-only">Previous</span>
 					</a>
 					<a
-						class="carousel-control-next"
+						className="carousel-control-next"
 						href="#carouselExampleControls"
 						role="button"
 						data-slide="next"
 					>
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
+						<span
+							className="carousel-control-next-icon"
+							aria-hidden="true"
+						></span>
+						<span className="sr-only">Next</span>
 					</a>
 				</div>
 			</>
