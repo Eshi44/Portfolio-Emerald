3ea47839d5295086e1838346694ef12f9dc202bb
Eshi44 <e.hameliervolino@gmail.com>
2020-05-26 14:40:45 -0400

    added carousel img info

--- client/src/containers/Portfolio/Portfolio.jsx
+++ client/src/containers/Portfolio/Portfolio.jsx
@@ -7,23 +7,36 @@ import EatDaBurger from "../../images/eat-da-burger.png";
 import ConcertHunt from "../../images/concerthunt.png";
 import EmployeeDir from "../../images/employee-directory.png";
 import TeamProfileGen from "../../images/team-profile-generator.png";
+import $ from "jquery";
 import "./Portfolio.css";
 
 class Portfolio extends Component {
-	componentDidMount() {}
+	componentDidMount() {
+
+	}
 
 	render() {
 		return (
 			<>
 				<div className="container-fluid" id="portfolio-container">
 					<h1 id="center-header-section-text">Portfolio</h1>
-					<Carousel slidesPerScroll={1} slidesPerPage={3} infinite arrows  centered>
-					<a href="" target="_blank"><img id="third-wave-coffee-img" src={PokeBattle} /> </a>
-					<a href="" target="_blank"><img id="third-wave-coffee-img" src={ConcertHunt} /> </a>
-					<a href="" target="_blank"><img id="third-wave-coffee-img" src={EatDaBurger} /> </a>
-					<a href="" target="_blank"><img id="third-wave-coffee-img" src={EmployeeDir} /> </a>
-					<a href="" target="_blank"><img id="third-wave-coffee-img" src={TeamProfileGen} /> </a>
-					<a href="" target="_blank"><img id="third-wave-coffee-img" src={ThirdWaveCoffee} /> </a>
+					<Carousel slidesPerScroll={1} slidesPerPage={3} infinite arrows  centered
+					 breakpoints={{
+						640: {
+						  slidesPerPage: 1,
+						  arrows: true
+						},
+						900: {
+						  slidesPerPage: 3,
+						  arrows: true
+						}
+					  }}>
+					<a href="" target="_blank"><img id="carousel-img" src={PokeBattle} /></a>
+					<a href="" target="_blank"><img id="carousel-img" src={ConcertHunt} /> </a>
+					<a href="" target="_blank"><img id="carousel-img" src={EatDaBurger} /> </a>
+					<a href="" target="_blank"><img id="carousel-img" src={EmployeeDir} /> </a>
+					<a href="" target="_blank"><img id="carousel-img" src={TeamProfileGen} /> </a>
+					<a href="" target="_blank"><img id="carousel-img" src={ThirdWaveCoffee} /> </a>
 					</Carousel>
 				</div>
 
