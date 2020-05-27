914c02b482a57ccaaa1b4055261733e7f5f5ffb8
Eshi44 <e.hameliervolino@gmail.com>
2020-05-27 14:01:36 -0400

    added team generator page

--- client/src/containers/Portfolio-Pages/TeamGenerator.jsx
+++ client/src/containers/Portfolio-Pages/TeamGenerator.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import NavBar from "../../components/NavBar/NavBar";
-
+import GeneratorMain from "../../images/team-profile-generator-screenshot.png";
 import "./Pages.css";
 
 class TeamGenerator extends Component {
-    render() {
+	render() {
 		return (
 			<>
 				<NavBar />
 				<div className="container" id="gray-bkgd">
 					<div className="row" id="margin-space">
 						<div className=" col-md-5 col-lg-5 col-xl-5">
-							<img id="page-portfolio-img" src={ThirdWaveMain} />
+							<img id="page-portfolio-img" src={GeneratorMain} />
 						</div>
-                        <div className=" col-md-2 col-lg-2 col-xl-2"></div>
+						<div className=" col-md-2 col-lg-2 col-xl-2"></div>
 						<div className=" col-md-5 col-lg-5 col-xl-5">
 							<h1
 								id="title-of-page"
 								className="animate__animated animate__bounceInRight"
 							>
-								Third Wave Coffee
+								Team Profile Generator
 							</h1>
 
 							<button id="button-skills" type="button" class="btn btn-primary">
@@ -27,15 +27,6 @@ class TeamGenerator extends Component {
 							</button>
 							<button id="button-skills" type="button" class="btn btn-primary">
 								Bootstrap
-							</button>
-							<button id="button-skills" type="button" class="btn btn-primary">
-								Express
-							</button>
-							<button id="button-skills" type="button" class="btn btn-primary">
-								MongoDB
-							</button>
-							<button id="button-skills" type="button" class="btn btn-primary">
-								Mongoose ODM
 							</button>
 							<button id="button-skills" type="button" class="btn btn-primary">
 								HTML & CSS
@@ -47,29 +38,36 @@ class TeamGenerator extends Component {
 								Node.js
 							</button>
 							<button id="button-skills" type="button" class="btn btn-primary">
-								jQuery
+								Inquirer npm
 							</button>
 							<button id="button-skills" type="button" class="btn btn-primary">
-								REACT
+								Jest npm
 							</button>
 
 							<p id="info-on-project">
 								{" "}
-								This is a full stack MERN application with Full CRUD
-								Functionality that follows the MVC paradigm and authenticates
-								users. <p></p>
-								Users passwords are hashed as well as encrypted in a JSON Web
-								Token. Environmental variables are also hidden. Users can
-								securly login and keep track of their coffee creations.
+								A CLI application using Inquirer and TDD with Jest. <p></p>
+								The application is designed to build a software engineering team
+								generated through a node.js command line application. The
+								application will prompt the user for information about the team
+								and their information such as name, id, position, github, and
+								email. The user can input any number of team members, and they
+								may be a mix of engineers, managers and interns.
 							</p>
-							<a href="https://third-wave-coffee-app.herokuapp.com/" target="blank"><button id="button-links" type="button" class="btn btn-primary">Website</button> </a>
-                            <a href="https://github.com/Eshi44/Third-Wave-Coffee" target="blank"><button id="button-links" type="button" class="btn btn-primary">Github</button></a>
+							<a
+								href="https://github.com/Eshi44/Team-Profile-Generator"
+								target="blank"
+							>
+								<button id="button-links" type="button" class="btn btn-primary">
+									Github
+								</button>
+							</a>
 						</div>
 					</div>
 				</div>
 			</>
 		);
-    }
+	}
 }
 
 export default TeamGenerator;
