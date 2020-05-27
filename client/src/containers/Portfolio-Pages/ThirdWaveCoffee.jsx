84d560ac6b40211a31c8ce78df7d929cd11ca335
Eshi44 <e.hameliervolino@gmail.com>
2020-05-27 11:08:37 -0400

    added pill buttons for third wave container

--- client/src/containers/Portfolio-Pages/ThirdWaveCoffee.jsx
+++ client/src/containers/Portfolio-Pages/ThirdWaveCoffee.jsx
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import ThirdWave from "../../images/third-wave-coffee.png";
+import ThirdWaveMain from "../../images/third-wave.jpg";
+import "./Pages.css";
 
 class ThirdWaveCoffee extends Component {
-    render() {
-        return (
-            <div>
-                <h1>hi</h1>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<>
+				<NavBar />
+				<div className="container">
+					<div className="row" id="margin-space">
+						<div className=" col-md-6 col-lg-6 col-xl-6">
+						
+							<img id="third-wave-img" src={ThirdWaveMain} />
+						</div>
+
+						<div className=" col-md-6 col-lg-6 col-xl-6">
+
+                 
+                   <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button>
+                        <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button>
+                        <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button>
+                        <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button>
+                        <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button>
+                        <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button>
+                        <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button>
+                        <button id="button-skills" type="button" class="btn btn-primary">MongoDB</button> 
+
+                        </div>
+                     
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default ThirdWaveCoffee;
